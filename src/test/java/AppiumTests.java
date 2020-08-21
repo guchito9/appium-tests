e7d77123e1ab9c7f83e5761229f726fee2929722
Guche <noel422@gmail.com>
2020-08-21 02:34:48 -0300

    Working basic one click test

--- src/test/java/AppiumTests.java
+++ src/test/java/AppiumTests.java
@@ -1,15 +1,17 @@
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.function.Function;
 
 public class AppiumTests {
 
@@ -20,44 +22,30 @@ public class AppiumTests {
     String secondNewJob = "//android.widget.FrameLayout[2]/android.widget.LinearLayout/" +
             "android.widget.RelativeLayout/android.widget.ImageView";
 
-    @BeforeMethod
+    @Before
     public void setup () throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "Galaxy Nexus API 24");
+        //caps.setCapability("deviceName", "Galaxy Nexus API 24");
         caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
         caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "7.0");
+        caps.setCapability("platformVersion", "10.0");
         caps.setCapability("skipUnlock","true");
-        caps.setCapability("appPackage", "com.isinolsun.app");
-        caps.setCapability("appActivity","com.isinolsun.app.activities.SplashActivity");
+        caps.setCapability("appPackage", "com.mercadopago.example");
+        caps.setCapability("appActivity","com.mercadopago.CheckoutExampleActivity");
         caps.setCapability("noReset","false");
-        driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),caps);
+        driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
         wait = new WebDriverWait(driver, 10);
     }
 
 
     @Test
-    public void basicTest () throws InterruptedException {
-        //Click and pass Splash
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.isinolsun.app:id/animation_view"))).click();
-
-        //Click I am searching a job
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.id("com.isinolsun.app:id/bluecollar_type_button"))).click();
-
-
-        //Notification Allow
-        if (driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).size()>0) {
-            driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).get(0).click();
-        }
-
-        wait.until(ExpectedConditions.visibilityOfElementLocated
-                (By.xpath(secondNewJob)));
+    public void basicTest() {
+        //Click button
+        driver.findElementById("customInitializeButton").click();
     }
 
-    @AfterMethod
-    public void teardown(){
-        driver.quit();
+    @After
+    public void teardown() {
+        //driver.quit();
     }
 }
